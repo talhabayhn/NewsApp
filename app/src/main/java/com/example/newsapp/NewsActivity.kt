1dd3fafa8ec7826e371aed7c57838ef93e2f68b1
INNOVA\tbayhan <hbayhan@innova.com.tr>
2024-03-13 16:53:36 +0300

    *retrofit setup

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -1,4 +1,4 @@
-package com.example.newsapp.ui
+package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "014a16c99db84108be9b8a926ca176f2"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
