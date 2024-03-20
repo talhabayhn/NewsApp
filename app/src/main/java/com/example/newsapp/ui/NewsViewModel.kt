15330d4bef6b3ed06cfd8d5d2aaf792645a0ae5d
INNOVA\tbayhan <hbayhan@innova.com.tr>
2024-03-20 13:10:28 +0300

    *view model & resource class added

---
+++ app/src/main/java/com/example/newsapp/ui/NewsViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.newsapp.repository.NewsRepository
+
+class NewsViewModel(
+    repository: NewsRepository
+) : ViewModel() {
+
+
+}
