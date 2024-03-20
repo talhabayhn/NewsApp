c122bd54aff3e6d07ce8a4ed5d2e9cb5c6b51ce6
INNOVA\tbayhan <hbayhan@innova.com.tr>
2024-03-20 11:10:38 +0300

    *room - dao - recyclerView added*

---
+++ app/src/main/java/com/example/newsapp/db/Converters.kt
@@ -0,0 +1,17 @@
+package com.example.newsapp.db
+
+import androidx.room.TypeConverter
+import com.example.newsapp.api.Source
+
+class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source):String{   // Room cannot use complicated class , like Source
+        return source.name                  // Source class converted into String
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source{
+        return Source(name,name)
+    }
+}
